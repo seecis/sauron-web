93e03f7bc6804547e71e0deb4a977fc193cc94da
Yiğit Ünlü <yigit.unlu@etts.com.tr>
2018-07-17 12:11:16 +0300

    Remove unused package from prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,7 +2,6 @@ process.env.NODE_ENV = 'production';
 
 const autoprefixer = require('autoprefixer');
 const path = require('path');
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./config/paths');
 const getClientEnvironment = require('./config/env');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -50,7 +49,6 @@ module.exports = {
             // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
             // please link the files into your node_modules/ and let module-resolution kick in.
             // Make sure your source files are compiled, as they will not be processed in any way.
-            new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
         ],
     },
     module: {
