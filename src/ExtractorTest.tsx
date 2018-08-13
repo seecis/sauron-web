66f21f38ce5d13bee24553c52c2c51d7226fdbe6
Yiğit Ünlü <yigit.unlu@etts.com.tr>
2018-08-13 14:59:30 +0300

    Fix build

--- src/ExtractorTest.tsx
+++ src/ExtractorTest.tsx
@@ -1,25 +1,25 @@
-import * as React from "react";
-import {ExtractorView} from "./ExtractorExpansionPanel";
-import Query from "./models";
-
-class ExtractorTest extends React.Component<any, any> {
-    render() {
-        return <ExtractorView
-            address={"a"}
-            depth={1}
-            editAddress={null}
-            extractor={new Query("a", "b")}
-            hoverCallback={(h) => {}}
-            index={1}
-            onChange={() => {}}
-            onCheckedChange={() => {}}
-            onDelete={() => {}}
-            onEditAddressSet={() => {}}
-            onEditComplete={() =>{} }
-            onExtracorLabelChange={(s, q) => {}}
-            parentValue={}/>
-    }
-}
-
-
-export default ExtractorTest;
+// import * as React from "react";
+// import {ExtractorView} from "./ExtractorExpansionPanel";
+// import Query from "./models";
+//
+// class ExtractorTest extends React.Component<any, any> {
+//     render() {
+//         return <ExtractorView
+//             address={"a"}
+//             depth={1}
+//             editAddress={null}
+//             extractor={new Query("a", "b")}
+//             hoverCallback={(h) => {}}
+//             index={1}
+//             onChange={() => {}}
+//             onCheckedChange={() => {}}
+//             onDelete={() => {}}
+//             onEditAddressSet={() => {}}
+//             onEditComplete={() =>{} }
+//             onExtracorLabelChange={(s, q) => {}}
+//             parentValue={}/>
+//     }
+// }
+//
+//
+// export default ExtractorTest;
